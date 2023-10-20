c6e1e2312c7714616df7ab50ebd2ca25443f55e1
James <jamesrwyatt2@gmail.com>
2023-10-20 15:39:43 -0400

    basic layout

--- App.js
+++ App.js
@@ -1,33 +1,43 @@
-import { StatusBar } from 'expo-status-bar';
 import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.appContainer}>
-    <View style={styles.inputContainer}>
-      <TextInput placeholder='Course Goal' style={styles.TextInput} />
-      <Button title='Add Goal' />
-    </View>
-    <View>
-      <Text>List of Goals...</Text>
-    </View>
+      <View style={styles.inputContainer}>
+        <TextInput placeholder='Course Goal' style={styles.textInput} />
+        <Button title='Add Goal' />
+      </View>
+      <View style={styles.goalsContainer}>
+        <Text>List of Goals...</Text>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   appContainer: {
-    padding: 50,
+    flex: 1,
+    paddingTop: 50,
+    paddingHorizontal: 16,
+
   },
   inputContainer: {
+    flex: 1,
     flexDirection: 'row',
     justifyContent: 'space-between',
+    alignItems: 'center',
+    paddingBottom: 24,
+    borderBottomWidth: 1,
+    borderBottomColor: 'black',
   },
-  TextInput: {
-    width: '80%',
+  textInput: {
+    width: '70%',
     borderColor: 'black',
     borderWidth: 1,
     marginRight: 10,
     padding: 10,
+  },
+  goalsContainer: {
+    flex: 3,
   }
 });
