e01d970d2bc20829004f1fc94179acd4cc94e237
James <jamesrwyatt2@gmail.com>
2023-10-20 16:44:35 -0400

    working add goal and display

--- App.js
+++ App.js
@@ -1,14 +1,34 @@
 import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
+import { useState } from 'react';
 
 export default function App() {
+  const [enteredGoal, setEnteredGoal] = useState('');
+  const [courseGoals, setCourseGoals] = useState([]);
+
+  function goalInputHandler(enteredText) {
+    console.log(enteredText);
+    setEnteredGoal(enteredText);
+  }
+  function addGoalHandler() {
+    setCourseGoals(currentGoals => [...currentGoals, enteredGoal]);
+    console.log(courseGoals);
+  }
   return (
     <View style={styles.appContainer}>
       <View style={styles.inputContainer}>
-        <TextInput placeholder='Course Goal' style={styles.textInput} />
-        <Button title='Add Goal' />
+        <TextInput
+          placeholder='Course Goal'
+          style={styles.textInput}
+          onChangeText={goalInputHandler} />
+        <Button title='Add Goal'
+          onPress={addGoalHandler} />
       </View>
       <View style={styles.goalsContainer}>
-        <Text>List of Goals...</Text>
+
+        {courseGoals.map((goal) =>
+          <Text key={goal}>{goal}</Text>
+        )}
+
       </View>
     </View>
   );
